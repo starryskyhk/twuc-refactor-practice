6eaca0694c95e88fec86c94a37b5deeb80463688
HanKun <1545731887@qq.com>
2021-08-12 00:48:34 +0800

    format code

--- 1/src/main/java/com/twu/refactoring/LineItem.java
+++ 1/src/main/java/com/twu/refactoring/LineItem.java
@@ -1,36 +1,36 @@
 package com.twu.refactoring;
 
 public class LineItem {
-	private final String describe;
-	private final double price;
-	private final int quantity;
+    private final String describe;
+    private final double price;
+    private final int quantity;
 
-	public LineItem(String describe, double price, int quantity) {
-		super();
-		this.describe = describe;
-		this.price = price;
-		this.quantity = quantity;
-	}
+    public LineItem(String describe, double price, int quantity) {
+        super();
+        this.describe = describe;
+        this.price = price;
+        this.quantity = quantity;
+    }
 
-	public String getDescription() {
-		return describe;
-	}
+    public String getDescription() {
+        return describe;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    public double getPrice() {
+        return price;
+    }
 
-	public int getQuantity() {
-		return quantity;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
     double totalAmount() {
         return price * quantity;
     }
 
-	public String getLineItem() {
-		return describe+'\t'+ price+'\t'+ quantity+ '\t' + totalAmount()+'\n';
-	}
+    public String getLineItem() {
+        return describe + '\t' + price + '\t' + quantity + '\t' + totalAmount() + '\n';
+    }
 
     public double getDiscountPrice() {
         return totalAmount() * .10;
--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -5,24 +5,23 @@ package com.twu.refactoring;
  * price and amount. It also calculates the sales tax @ 10% and prints as part
  * of order. It computes the total order amount (amount of individual lineItems +
  * total sales tax) and prints it.
- *
  */
 public class OrderReceipt {
-	public static final String HEAD = "======Printing Orders======\n";
-	public static final String SALES_TAX = "Sales Tax";
-	public static final String TOTAL_AMOUNT = "Total Amount";
-	private final Order order;
+    public static final String HEAD = "======Printing Orders======\n";
+    public static final String SALES_TAX = "Sales Tax";
+    public static final String TOTAL_AMOUNT = "Total Amount";
+    private final Order order;
 
     public OrderReceipt(Order order) {
         this.order = order;
-	}
+    }
 
-	public String printReceipt() {
+    public String printReceipt() {
 
-		return HEAD +
-			order.getBody() +
-			SALES_TAX + '\t' + order.getTotSalesTx() +
-			TOTAL_AMOUNT + '\t' + order.getTotal();
-	}
+        return HEAD +
+            order.getBody() +
+            SALES_TAX + '\t' + order.getTotSalesTx() +
+            TOTAL_AMOUNT + '\t' + order.getTotal();
+    }
 
 }
