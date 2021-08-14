844a97cb4155794fea4e4a108166f3b7a0ffd646
HanKun <1545731887@qq.com>
2021-08-14 23:59:49 +0800

    extract method

--- 5/src/main/java/com/twu/refactoring/Receipt.java
+++ 5/src/main/java/com/twu/refactoring/Receipt.java
@@ -16,16 +16,34 @@ public class Receipt {
         totalCost += FIXED_CHARGE.getValue();
 
         // taxi charges
-        int totalKms = taxi.getTotalKms();
-        double peakTimeMultiple = taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER.getValue() : OFF_PEAK_MULTIPLIER.getValue();
+        double peakTimeMultiple = getPeakTimeMultiple();
         if(taxi.isAirConditioned()) {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE.getValue(), totalKms) * PRE_RATE_CHANGE_AC_RATE.getValue() * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE.getValue()) * POST_RATE_CHANGE_AC_RATE.getValue() * peakTimeMultiple;
+            totalCost += haveAirConditioned(peakTimeMultiple);
         } else {
-            totalCost += Math.min(RATE_CHANGE_DISTANCE.getValue(), totalKms) * PRE_RATE_CHANGE_NON_AC_RATE.getValue() * peakTimeMultiple;
-            totalCost += Math.max(0, totalKms - RATE_CHANGE_DISTANCE.getValue()) * POST_RATE_CHANGE_NON_AC_RATE.getValue() * peakTimeMultiple;
+            totalCost += notAirConditioned(peakTimeMultiple);
         }
 
+        return getResult(totalCost);
+    }
+
+    private Double getPeakTimeMultiple() {
+        return taxi.isPeakTime() ? PEAK_TIME_MULTIPLIER.getValue() : OFF_PEAK_MULTIPLIER.getValue();
+    }
+
+    private double getResult(double totalCost) {
         return totalCost * (1 + SALES_TAX_RATE.getValue());
+    }
+
+    public double haveAirConditioned(double peakTimeMultiple) {
+        int totalKms = taxi.getTotalKms();
+        return Math.min(RATE_CHANGE_DISTANCE.getValue(), totalKms) * PRE_RATE_CHANGE_AC_RATE.getValue() * peakTimeMultiple
+        + Math.max(0, totalKms - RATE_CHANGE_DISTANCE.getValue()) * POST_RATE_CHANGE_AC_RATE.getValue() * peakTimeMultiple;
+    }
+
+    public double notAirConditioned(double peakTimeMultiple) {
+        int totalKms = taxi.getTotalKms();
+        return Math.min(RATE_CHANGE_DISTANCE.getValue(), totalKms) * PRE_RATE_CHANGE_NON_AC_RATE.getValue() * peakTimeMultiple
+        +Math.max(0, totalKms - RATE_CHANGE_DISTANCE.getValue()) * POST_RATE_CHANGE_NON_AC_RATE.getValue() * peakTimeMultiple;
+
     }
 }
